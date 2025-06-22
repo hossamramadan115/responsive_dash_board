3771734df2b91cd0e24a5f9d6dcffe25fffceec8
hossam ramadan <hossamramadan115@gmail.com>
2025-06-22 17:04:31 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => ResponsiveDashBoard(),
+    ),
+  );
+}
+
+class ResponsiveDashBoard extends StatelessWidget {
+  const ResponsiveDashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      home: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Responsive text example'),
+      ),
+      body:
+    );
+  }
+}
+
+// double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
+//   double scaleFactor = getScaleFactor(context);
+//   double responsiveFontSize = fontSize * scaleFactor;
+
+//   double lowerLimit = fontSize * .8;
+//   double upperLimit = fontSize * 1.2;
+//   return responsiveFontSize.clamp(lowerLimit, upperLimit);
+// }
+
+// double getScaleFactor(BuildContext context) {
+//   double width = MediaQuery.sizeOf(context).width;
+//   if (width < 600) {
+//     return width / 400;
+//   } else if (width < 900) {
+//     return width / 700;
+//   } else {
+//     return width / 1000;
+//   }
+// }
